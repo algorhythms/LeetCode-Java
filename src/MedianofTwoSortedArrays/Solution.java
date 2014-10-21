9591ac34e08dbf6c3b148ca9d3baf23f45b68a08
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2014-10-22 00:00:32 +0800

    init

---
+++ src/MedianofTwoSortedArrays/Solution.java
@@ -0,0 +1,14 @@
+package MedianofTwoSortedArrays;
+
+/**
+ * “Measuring programming progress by lines of code is like measuring aircraft building progress by weight.”
+ * - Bill Gates
+ * User: Danyang
+ * Date: 10/21/14
+ * Time: 10:45 PM
+ */
+public class Solution {
+    public double findMedianSortedArrays(int A[], int B[]) {
+        return 0.0;
+    }
+}
